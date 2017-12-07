7ac3126ac3336f6e653591caf9b14791c4661b5a
ahmadkikok <ahmadshidqifirdaus@gmail.com>
2017-12-07 21:49:34 +0700

    Need more info about truncate.

--- truncate.c
+++ truncate.c
@@ -3,23 +3,34 @@
 #include "user.h"
 #include "fcntl.h"
 
+char buf[512];
+
 int
 main(int argc, char *argv[])
 {
-  int i;
+
+int n, fd0;
 
   if(argc < 2){
     printf(2, "Usage: truncate files...\n");
     exit();
   }
+  
+  if(argc > 1){
+    printf(2, "Usage: truncate files...\n");
+  }
 
-  for(i = 1; i < argc; i++){
-    if(argv[i] < 0){
-      printf(2, "truncate: %s failed to truncate\n", argv[i]);
-      break;
-    }
-	unlink(argv[2]);
-	open(argv[2], O_CREATE);
+  if(open(argv[1],O_RDONLY) < 0){
+    printf(1, "truncate: file %s not found.\n", argv[1]);
+    exit();
+  }
+
+    unlink(argv[1]);
+    fd0=open(argv[1], O_CREATE);    
+
+
+  while ( ( n = read (fd0, buf, sizeof(buf))) > 0 ){
+	write ( fd0, buf, n );
   }
 
   exit();
