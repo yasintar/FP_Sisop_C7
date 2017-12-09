56976fc6f0ff2ed183b5a7d4064596edbec7b179
sultanabalqis <sultana.balqis@gmail.com>
2017-12-09 13:22:36 +0800

    truncate size file

--- truncate.c
+++ truncate.c
@@ -3,33 +3,32 @@
 #include "user.h"
 #include "fcntl.h"
 
-char buf[512];
-
 int
 main(int argc, char *argv[])
 {
 
-int n, fd0;
+int n, fd0, fd1;
 
-  if(argc < 2){
-    printf(2, "Usage: truncate files...\n");
+char buf[strlen(argv[2])];
+
+  if(argc < 3){
+    printf(2, "Usage: truncate size files...\n");
     exit();
   }
   
-  if(argc > 1){
-    printf(2, "Usage: truncate files...\n");
+  if(argc > 3){
+    printf(2, "Usage: truncate size files...\n");
   }
 
-  if(open(argv[1],O_RDONLY) < 0){
+  if(open(argv[2],O_RDONLY) < 0){
     printf(1, "truncate: file %s not found.\n", argv[1]);
     exit();
   }
 
-    unlink(argv[1]);
-    fd0=open(argv[1], O_CREATE);    
+    fd0=open(argv[2], O_CREATE|O_RDWR);    
+    fd1= *argv[2];
 
-
-  while ( ( n = read (fd0, buf, sizeof(buf))) > 0 ){
+  while ( ( n = read (fd0, buf, fd1)) > 0 ){
 	write ( fd0, buf, n );
   }
 
